b90fdb2570e8ea1150a1db0d5b6ed41c9ea25e3a
Mazhar Hassan <netsurgeon@gmail.com>
2024-01-01 18:30:18 +0500

    Global Swagger documentation added to show response messages

--- java-api/src/main/java/com/api/tutorials/configs/OpenApiConfig.java
+++ java-api/src/main/java/com/api/tutorials/configs/OpenApiConfig.java
@@ -1,12 +1,20 @@
 package com.api.tutorials.configs;
 
+import com.api.tutorials.dtos.ExceptionResponse;
+import io.swagger.v3.core.converter.AnnotatedType;
+import io.swagger.v3.core.converter.ModelConverters;
+import io.swagger.v3.core.converter.ResolvedSchema;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.media.Content;
+import io.swagger.v3.oas.models.media.MediaType;
+import io.swagger.v3.oas.models.responses.ApiResponse;
 import io.swagger.v3.oas.models.servers.Server;
+import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -40,5 +48,18 @@ public class OpenApiConfig {
 
         return new OpenAPI().info(info).
                 addServersItem(localServer);
+    }
+
+    @Bean
+    public OpenApiCustomizer openApiCustomizer() {
+        ResolvedSchema errResSchema = ModelConverters.getInstance()
+                .resolveAsResolvedSchema(new AnnotatedType(ExceptionResponse.class));
+
+        Content content = new Content().addMediaType("application/json", new MediaType().schema(errResSchema.schema));
+        return openApi -> openApi.getPaths().values()
+                        .forEach(pathItem -> pathItem.readOperations()
+                                .forEach(operation -> operation.getResponses()
+                                        .addApiResponse("400", new ApiResponse().description("Bad Request").content(content))
+                                        .addApiResponse("404", new ApiResponse().description("Resource Not Found").content(content))));
     }
 }
